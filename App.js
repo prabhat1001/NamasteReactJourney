23b681fbe4fbdb98e37e0a2731b8a2748423782f
prabhat1001 <prabhatsaraswat24@gmail.com>
2024-01-01 13:19:48 +0530

    Refactor App.js and index.css

--- App.js
+++ App.js
@@ -1,36 +1,20 @@
 import React from "react"; //importing react from node_modules
 import ReactDOM from "react-dom/client";
 
-// React.createElement  =>  Object  =>  HTML Element ( on render )
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World! 🚀 "
-// );
-
-
-// JSX => JavaScript XML a syntax extension to JavaScript
-
-const heading = (
+const Title = () => (
   <h1 className="head" tabIndex="5">
-    Hello World! using JSX 🚀 
-  </h1>
+    Namaste React Using JSX 🚀
+  </h1>  
 );
 
-//React Components
-
-// Class Based Components
-// Functional Components
-
-const HeaderComponent = () => {
-  return <h1>Namaste React Functional Component! 🚀 </h1>;
-};
-
-//Another way of writing the above code
-const HeaderComponent2 = () => <h1 className="header">Namaste React Functional Component! 🚀 </h1>;
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1>Namaste React Functional Component</h1>
+  </div>
+);
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
