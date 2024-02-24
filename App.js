d8a59d3539abb865465aa4e66d90b73c6d733e88
prabhat1001 <prabhatsaraswat24@gmail.com>
2024-02-24 19:34:44 +0530

    used optional chaining for code cleanliness

--- App.js
+++ App.js
@@ -1545,26 +1545,30 @@ const HeaderComp = () => {
 
 const RestCards = (props) => {
   const {resData} = props;
+
+  const {cloudinaryImageId, name, avgRating, address, deliveryTime} = resData?.data;
+
   const maxLength = 60; // Maximum length for the text
   const truncateText = (text) => {
     return text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
   };
+
   // console.log(resData.data.name);
   return (
     <div className="card">
       <div className="img-div">
-        <img src= {`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`+ resData.data.cloudinaryImageId} alt="dish-img"></img>
+        <img src= {`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`+ cloudinaryImageId} alt="dish-img"></img>
       </div>
       <div className="detail-cont">
-        <h4>{resData.data.name}</h4>
+        <h4>{name}</h4>
         <div className="rating-cont">
           <img
             className="star"
             src="https://img.icons8.com/flat-round/64/000000/star--v1.png"
           ></img>
-          <p>{resData.data.avgRating}</p>
+          <p>{avgRating} • {deliveryTime} min</p>
         </div>
-        <p className="address">{truncateText(resData.data.address)}</p>
+        <p className="address">{truncateText(address)}</p>
       </div>
     </div>
   );
