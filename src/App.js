55cbb132db63f780c8f60e59f51027d0c1e6b38e
prabhat1001 <prabhatsaraswat24@gmail.com>
2024-03-15 12:20:32 +0530

    Imported Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"; 
 import ReactDOM from "react-dom/client";
 
-
+//Imported components
 import Header from "./components/Header"
 import Body from "./components/Body"
 import Footer from "./components/Footer"
