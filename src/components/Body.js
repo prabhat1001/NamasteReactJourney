71fd41e8e68498b54e54785b5c89f1a92bb8988d
prabhat1001 <prabhatsaraswat24@gmail.com>
2024-03-15 12:46:09 +0530

    remove restCards and import it from a seperate file for `RestaurantCards`

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import RestCards from './RestaurantCards';
 
 //Dummy data from swiggy API ( for practice )
 const resObj = [
@@ -1627,41 +1628,6 @@ const resObj = [
       subtype: "basic",
     },
   ]
-  
-const RestCards = (props) => {
-    const {resData} = props;
-  
-    const {cloudinaryImageId, name, avgRating, address, deliveryTime} = resData?.data;
-  
-    const maxLength = 60; // Maximum length for the text
-    const truncateText = (text) => {
-      return text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
-    };
-  
-    // console.log(resData.data.name);
-    return (
-      <div className="card">
-        <div className="img-div">
-          <img src= {`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`+ cloudinaryImageId} alt="dish-img"></img>
-        </div>
-        <div className="detail-cont">
-          <h4>{name}</h4>
-          <div className="rating-cont">
-            <img
-              className="star"
-              src="https://img.icons8.com/flat-round/64/000000/star--v1.png"
-            ></img>
-            <p>{avgRating} • {deliveryTime} min</p>
-          </div>
-          <p className="address">{truncateText(address)}</p>
-        </div>
-      </div>
-    );
-};
-
-// not using keys (not acceptable) <<< using index as keys <<< using Unique Id's as keys (most preferable way).
-
-// (worst practice) --------------------------------> (best practice)
 
 const Body = () => {
     return (
