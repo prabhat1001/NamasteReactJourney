06ff25d0eec03ee7cb2a906790b16bd5804f4a5c
prabhat1001 <prabhatsaraswat24@gmail.com>
2024-03-15 16:56:31 +0530

    created `constant.js` for storing constant values and urls inside utilities

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const STAR_URL = "https://img.icons8.com/flat-round/64/000000/star--v1.png"
+
+
+
